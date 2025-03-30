4eb39edb1973920299a8d7871e9c49f8ad3d58c3
zongshun <zhangzs@bu.edu>
2025-03-30 21:55:55 +0000

    Let new VM automatically pull changes from the remote repository

--- deploy/user_data.sh
+++ deploy/user_data.sh
@@ -3,26 +3,30 @@
 # Enable error handling
 set -e
 
-touch ~/user_data_script_executed
+# touch ~/user_data_script_executed
 
 # Set up logging to home directory
 exec 1> >(tee -a ~/user_data.log) 2>&1
 
-# Update and install packages for Ubuntu
-sudo apt-get update -y
-sudo apt-get install -y python3-pip git
+# # Update and install packages for Ubuntu
+# sudo apt-get update -y
+# sudo apt-get install -y python3-pip git
 
-# Clone the repository
-REPO_URL="https://github.com/Zongshun96/ml-model-deployment.git"
+# # Clone the repository
+# REPO_URL="https://github.com/Zongshun96/ml-model-deployment.git"
 REPO_PATH="/home/ubuntu/ml-model-deployment"
 
-echo "Cloning repository..."
-git clone "$REPO_URL" "$REPO_PATH"
+# echo "Cloning repository..."
+# git clone "$REPO_URL" "$REPO_PATH"
 
-# Install Python packages
-echo "Installing Python requirements..."
+# # Install Python packages
+# echo "Installing Python requirements..."
 cd "$REPO_PATH"
-sudo pip3 install -r requirements.txt
+# sudo pip3 install -r requirements.txt
+
+# Pull the latest changes
+echo "Pulling the changes..."
+git pull origin main
 
 # Run the application
 echo "Starting the application..."
