6529b5399df551185d4d671cb2e91b1a15c7e043
zongshun <zhangzs@bu.edu>
2025-05-15 18:22:10 +0000

    Fix health check route

--- src_ss/app.py
+++ src_ss/app.py
@@ -293,6 +293,10 @@ load_models()
 # -----------------------------
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def health_check():
+    return jsonify({'status': 'healthy'}), 200
+
 @app.route('/predict', methods=['POST'])
 def predict():
     """
